365cd27f1b71e9f658a04d91774eeff7b16bb122
Negin Kordinejad <negin.kordinejad@jayride.com>
2024-02-02 16:20:43 +1100

    refactor feeding dbcontext.

--- Zip.WebAPI/Startup.cs
+++ Zip.WebAPI/Startup.cs
@@ -26,14 +26,11 @@ namespace Zip.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var contextOptions = new DbContextOptionsBuilder<ZipUserDBContext>()
-                .UseSqlite(Configuration.GetConnectionString("DefaultConnection")).Options;
-
-            var dbContext = new ZipUserDBContext(contextOptions);
+            var contextOptions = new DbContextOptionsBuilder<ZipUserDBContext>().UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            var opt = contextOptions.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).Options;
+            var dbContext = new ZipUserDBContext(opt);
             dbContext.Database.OpenConnection();
-            dbContext.Database.EnsureDeleted();
             dbContext.Database.EnsureCreated();
-            dbContext.Database.OpenConnection();
             dbContext.Users.AddRange(UserDataProvider.Get());
             dbContext.SaveChanges();
 
